7f099986a61e02ad4b57cdf9964a4f51c3334454
adilkan <adilkanio555@gmail.com>
2022-03-30 23:28:22 +0600

    secon2d

--- handers/inline.py
+++ handers/inline.py
@@ -1,15 +1,35 @@
 import hashlib
-from aiogram import types,Dispatcher
+from aiogram import types, Dispatcher
 from youtube_search import YoutubeSearch
 
-async def inline_google(query: types.InlineQuery):
+# def finder(text):
+#     result = YoutubeSearch(text, max_results=10).to_dict()
+#     return result
+#
+# async def inline_youtube_handler(query: types.InlineQuery):
+#     text = query.query or "echo"
+#     links = finder(text)
+#     articles = [
+#         types.InlineQueryResultArticle(
+#             id=hashlib.md5(f"{link['id']}".encode()).hexdigest(),
+#             title=f"{link['title']}",
+#             url=f"https://youtube.com/watch?v={link['id']}",
+#             thumb_url=f"{link['thumbnails'][0]}",
+#             input_message_content=types.InputTextMessageContent(
+#                 message_text=f"https://youtube.com/watch?v={link['id']}"
+#             )
+#         ) for link in links
+#     ]
+#     await query.answer(articles, cache_time=60, is_personal=True)
+
+async def inline_wiki(query: types.InlineQuery):
     text = query.query or "echo"
     links= "https://www.google.com/search?q=" + text
     result_id: str = hashlib.md5(text.encode()).hexdigest()
     articles = [
         types.InlineQueryResultArticle(
             id=result_id,
-            title="Google:",
+            title="google:",
             url=links,
             input_message_content=types.InputTextMessageContent(
                 message_text=links
@@ -19,5 +39,7 @@ async def inline_google(query: types.InlineQuery):
     ]
     await query.answer(articles, cache_time=2, is_personal=True)
 
-def reqister(db: Dispatcher):
-    db.register_message_handler(inline_google)
+
+def register_handlers_inline(dp: Dispatcher):
+    # dp.register_inline_handler(inline_youtube_handler)
+    dp.register_inline_handler(inline_wiki)
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import asyncio
+
 from aiogram import executor
 from bot_instance import db
-from handers import client,callback, extra,hw3_fsmadmin,notification,hw_notification,inline
+from handers import client, callback, extra, fsmadmin, notification, inline
 from data_base import bot_db
 from handers.notification import schedukler
-from handers.hw_notification import remind
-
-
-
 
 
 
@@ -15,16 +12,16 @@ from handers.hw_notification import remind
 async def on_startup(_):
     bot_db.sql_create()
     asyncio.create_task(schedukler())
-    asyncio.create_task(remind())
+    print("Bot is online")
 
+
+fsmadmin.register_handler(db)
 client.register_handlers_client(db)
 callback.register_handler_callback(db)
-inline.reqister(db)
+inline.register_handlers_inline(db)
 extra.register_handlers_extra(db)
-hw_notification.register_notification(db)
+notification.register_notification(db)
 
 
-
-
-if __name__ == '__main__':
-    executor.start_polling(db,skip_updates=False,on_startup=on_startup)
+if __name__ == "__main__":
+    executor.start_polling(db, skip_updates=False, on_startup=on_startup)
