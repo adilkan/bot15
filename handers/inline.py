baca935bd1935054a0639e06ce2ca9fd279c93b7
adilkan <adilkanio555@gmail.com>
2022-04-01 09:33:57 +0600

    make it better

--- handers/inline.py
+++ handers/inline.py
@@ -1,26 +1,5 @@
 import hashlib
 from aiogram import types, Dispatcher
-from youtube_search import YoutubeSearch
-
-# def finder(text):
-#     result = YoutubeSearch(text, max_results=10).to_dict()
-#     return result
-#
-# async def inline_youtube_handler(query: types.InlineQuery):
-#     text = query.query or "echo"
-#     links = finder(text)
-#     articles = [
-#         types.InlineQueryResultArticle(
-#             id=hashlib.md5(f"{link['id']}".encode()).hexdigest(),
-#             title=f"{link['title']}",
-#             url=f"https://youtube.com/watch?v={link['id']}",
-#             thumb_url=f"{link['thumbnails'][0]}",
-#             input_message_content=types.InputTextMessageContent(
-#                 message_text=f"https://youtube.com/watch?v={link['id']}"
-#             )
-#         ) for link in links
-#     ]
-#     await query.answer(articles, cache_time=60, is_personal=True)
 
 async def inline_wiki(query: types.InlineQuery):
     text = query.query or "echo"
@@ -40,6 +19,5 @@ async def inline_wiki(query: types.InlineQuery):
     await query.answer(articles, cache_time=2, is_personal=True)
 
 
-def register_handlers_inline(dp: Dispatcher):
-    # dp.register_inline_handler(inline_youtube_handler)
-    dp.register_inline_handler(inline_wiki)
+def register_handlers_inline(db: Dispatcher):
+    db.register_inline_handler(inline_wiki)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import executor
 from bot_instance import db
-from handers import client,callback, extra,hw3_fsmadmin,notification,hw_notification,inline
+from handers import client,callback, extra ,hw_notification,inline
 from data_base import bot_db
 from handers.notification import schedukler
 from handers.hw_notification import remind
@@ -19,7 +19,7 @@ async def on_startup(_):
 
 client.register_handlers_client(db)
 callback.register_handler_callback(db)
-inline.reqister(db)
+inline.register_handlers_inline(db)
 extra.register_handlers_extra(db)
 hw_notification.register_notification(db)
 
