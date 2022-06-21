ef221dc2da63743f7cd1b281e9ad261198b1746e
adilkan <99826122+adilkan@users.noreply.github.com>
2022-06-22 01:56:33 +0600

    Update tv_show.py

--- parser/tv_show.py
+++ parser/tv_show.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://rt.potnhub.com"
+URL = "https://"
 
 HEADERS = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
